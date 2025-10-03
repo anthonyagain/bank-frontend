f67d2dc8332fca58d911bdaccc6b942f5ba8be5f
Anthony MacKay <anthonymackay@Mac.lan>
2025-10-02 21:46:52 -0500

    update url

--- vite-app/vite.config.ts
+++ vite-app/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bank-frontend/',
+  base: '/atm-app-frontend/',
   plugins: [
     tailwindcss(),
     react()
